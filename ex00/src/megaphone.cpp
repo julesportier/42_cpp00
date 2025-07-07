3019ef4c7567492a601542c1d29c6f814e3d20d8
jules <142106821+julesportier@users.noreply.github.com>
2025-07-07 17:43:52 +0200

    ref(ex00): remove useless arg size check (max defined in ARG_MAX by the system)

--- ex00/src/megaphone.cpp
+++ ex00/src/megaphone.cpp
@@ -10,40 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <limits>
 #include <iostream>
-
-static void	print_error(std::string message)
-{
-	std::cerr << "Error " + message + '\n';
-}
 
 static void	print_str_default()
 {
 	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 }
 
-static bool	is_max_int(int i)
-{
-	if (i == std::numeric_limits<int>::max())
-		return (true);
-	else
-		return (false);
-}
-
 static char	*to_upper_str(char *str)
 {
-	int	i = 0;
-
-	while (str[i] != '\0')
+	for (int i = 0; str[i] != '\0'; ++i)
 	{
-		if (is_max_int(i))
-		{
-			print_error("string is too big");
-			return (NULL);
-		}
 		str[i] = toupper(str[i]);
-		++i;
 	}
 	return (str);
 }
@@ -52,19 +30,16 @@ static int	print_str_upper(char *str)
 {
 	str = to_upper_str(str);
 	if (!str)
+	{
 		return (-1);
+	}
 	std::cout << str;
 	return (0);
 }
 
 int	main(int argc, char *argv[])
 {
-	if (is_max_int(argc))
-	{
-		print_error("too much arguments");
-		return (-1);
-	}
-	else if (argc == 1)
+	if (argc == 1)
 	{
 		print_str_default();
 	}
@@ -72,8 +47,7 @@ int	main(int argc, char *argv[])
 	{
 		for (int i = 1; argv[i]; ++i)
 		{
-			if (print_str_upper(argv[i]) == -1)
-				return (-1);
+			print_str_upper(argv[i]);
 		}
 		std::cout << '\n';
 	}

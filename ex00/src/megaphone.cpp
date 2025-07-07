a06cdc57b44ff2090d4fd31699677fe9af189def
jules <142106821+julesportier@users.noreply.github.com>
2025-07-07 12:08:21 +0200

    feat(ex00): full megaphone program

--- ex00/src/megaphone.cpp
+++ ex00/src/megaphone.cpp
@@ -10,16 +10,54 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <limits>
 #include <iostream>
+
+static bool	is_max_int(int i)
+{
+	if (i == std::numeric_limits<int>::max())
+		return (true);
+	else
+		return (false);
+}
+
+static void	print_str_default()
+{
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+}
+
+static char	*to_upper_str(char *str)
+{
+	int	i = 0;
+
+	while (str[i] != '\0') {
+		if (is_max_int(i))
+			throw std::string("string too big");
+		str[i] = toupper(str[i]);
+		++i;
+	}
+	return (str);
+}
+
+static void	print_str_upper(char *str)
+{
+	std::cout << to_upper_str(str);
+}
 
 int	main(int argc, char *argv[])
 {
-	std::string str;
-
-	if (argc > 1)
-	{
-		str = argv[1];
-		std::cout << str + "\n";
+	if (argc == 1)
+		print_str_default();
+	else {
+		for (int i = 1; argv[i]; ++i) {
+			try {
+				print_str_upper(argv[i]);
+			}
+			catch (std::string message) {
+				std::cout << "error " + message;
+			}
+		}
 	}
+	std::cout << std::endl;
 	return (0);
 }

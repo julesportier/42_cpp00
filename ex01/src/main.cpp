fe1dab234162eadf24a6d19d94cbcbef7f6aa84b
jules <142106821+julesportier@users.noreply.github.com>
2025-07-22 19:09:26 +0200

    feat(ex01): main loop

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,12 +1,40 @@
+#include <string>
 #include <iostream>
-#include "Contact.h"
 #include "PhoneBook.h"
+
+static void display_commands(void)
+{
+	std::cout << "type 'ADD' to add a contact\n"
+		"type 'SEARCH' to search for a contact\n"
+		"type 'EXIT' to exit the program\n";
+}
+
+static bool prompt_command(PhoneBook& phonebook)
+{
+	std::string input;
+	std::getline(std::cin, input);
+	if (input == "ADD") {
+		phonebook.add_contact();
+	}
+	else if (input == "SEARCH") {
+		phonebook.search_contact();
+	}
+	else if (input == "EXIT") {
+		return (false);
+	}
+	else {
+		std::cout << "Incorrect command\n";
+		display_commands();
+	}
+	return (true);
+}
 
 int main(void)
 {
-	PhoneBook pb;
-	
-	pb.add_contact();
-	pb.search_contact();
-	return (0);
+	display_commands();
+	PhoneBook phonebook;
+	while (1) {
+		if (prompt_command(phonebook) == false)
+			return (0);
+	}
 }

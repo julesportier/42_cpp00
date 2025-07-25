9fa4cf59beed8a6e08e3e24e4126e0710e119cdf
jules <142106821+julesportier@users.noreply.github.com>
2025-07-25 10:07:11 +0200

    ref(ex01): use this to access Contact instances

--- ex01/src/Contact.cpp
+++ ex01/src/Contact.cpp
@@ -6,50 +6,50 @@ Contact::~Contact() {}
 
 void Contact::set_first_name(std::string str)
 {
-	first_name = str;
+	this->first_name = str;
 }
 
 void Contact::set_last_name(std::string str)
 {
-	last_name = str;
+	this->last_name = str;
 }
 
 void Contact::set_nickname(std::string str)
 {
-	nickname = str;
+	this->nickname = str;
 }
 
 void Contact::set_phone_number(std::string str)
 {
-	phone_number = str;
+	this->phone_number = str;
 }
 
 void Contact::set_darkest_secret(std::string str)
 {
-	darkest_secret = str;
+	this->darkest_secret = str;
 }
 
 std::string Contact::get_first_name()
 {
-	return (first_name);
+	return (this->first_name);
 }
 
 std::string Contact::get_last_name()
 {
-	return (last_name);
+	return (this->last_name);
 }
 
 std::string Contact::get_nickname()
 {
-	return (nickname);
+	return (this->nickname);
 }
 
 std::string Contact::get_phone_number()
 {
-	return (phone_number);
+	return (this->phone_number);
 }
 
 std::string Contact::get_darkest_secret()
 {
-	return (darkest_secret);
+	return (this->darkest_secret);
 }
